461193873faebe888f995e3b01cbd72d00d64a8a
Gaurav Salunke <161368369+gaurav5327@users.noreply.github.com>
2025-05-06 11:08:53 +0530

    Update AdminRegistration.jsx

--- client/src/components/AdminRegistration.jsx
+++ client/src/components/AdminRegistration.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Navbar from "./Navbar";
 
-const API_URL = "http://localhost:3000/api/auth";
+const API_URL = "https://edusync-backend-gvgh.onrender.com/api/auth";
 
 function RegisterAdminPage() {
   const navigate = useNavigate();
