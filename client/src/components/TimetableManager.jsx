9a883a376ffce2cb2e645f85b326c43638c4bae7
Gaurav Salunke <161368369+gaurav5327@users.noreply.github.com>
2025-05-06 11:14:36 +0530

    Update TimetableManager.jsx

--- client/src/components/TimetableManager.jsx
+++ client/src/components/TimetableManager.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import ScheduleDisplay from "./ScheduleDisplay";
 
-const API_URL = "http://localhost:3000/api/schedule";
+const API_URL = "https://edusync-backend-gvgh.onrender.com/api/schedule";
 
 const YEARS = [1, 2, 3, 4];
 const BRANCHES = [
