37c68d50e9602d6503bad28993df8fe9da4adfe4
Gaurav Salunke <161368369+gaurav5327@users.noreply.github.com>
2025-05-06 11:09:30 +0530

    Update ConflictResolution.jsx

--- client/src/components/ConflictResolution.jsx
+++ client/src/components/ConflictResolution.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://edusync-backend-gvgh.onrender.com/api";
 
 function ConflictResolution() {
   const [conflicts, setConflicts] = useState([]);
